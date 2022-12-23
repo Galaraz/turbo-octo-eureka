706d8d7c11ed7515be7435e548c07f9573ba83cf
galaraz <jvictor.si.2013@gmail.com>
2022-12-23 10:54:02 -0400

    teste api 4

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 
 export default function Home(props) {
   const { destaques } = props
-  return null
   return (
     <>
      {destaques[0].id}
@@ -9,17 +8,18 @@ export default function Home(props) {
   )
 }
 
-// export async function getServerSideProps() {
-//   let resposta = []
-//   try{
-//     const urlLocal = "http://localhost:3000/"
-//     resposta = await fetch(urlLocal +  "api/home")
-//   }catch(e){
-//    const url = "https://turbo-octo-eureka.pages.dev/"
-//     resposta = await fetch(url +  "api/home")
-//   }
-//   const list = await resposta.json()
-//   return {
-//     props: list, 
-//   }
-// }
+export async function getServerSideProps() {
+  // let resposta = []
+  // try{
+  //   const urlLocal = "http://localhost:3000/"
+  //   resposta = await fetch(urlLocal +  "api/home")
+  // }catch(e){
+  //  const url = "https://turbo-octo-eureka.pages.dev/"
+  //   resposta = await fetch(url +  "api/home")
+  // }
+  // const list = await resposta.json()
+  const list = { destaques :[{id: 123}] }
+  return {
+    props: list, 
+  }
+}
