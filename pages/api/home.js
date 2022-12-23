ae143f06db6d78f755d2fd71105fa8b719a3ebc3
galaraz <jvictor.si.2013@gmail.com>
2022-12-23 10:40:37 -0400

    api teste

---
+++ pages/api/home.js
@@ -0,0 +1,25 @@
+const apiId = "992";
+const apiUrl = "https://dev.infoimoveis.com.br/webservice/hotsites.php";
+export default async  function requisicoes (req,res ) {
+  
+    const corpo = await JSON.stringify( {
+        acoes: [                        
+          { metodo: "destaques", params: [ { resultados: "4" }] },
+          { metodo: "ultimasnoticias", params: [ { resultados: "4" }] },
+        ], id: apiId
+      });
+    
+    const response =  await fetch(
+        apiUrl,
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: corpo
+        }
+    
+    );
+    const list = await response.json()
+
+  
+    res.status(200).json(list)
+}
