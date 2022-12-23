907fb269f18aadb6e71856a73a1c9b192722480a
galaraz <jvictor.si.2013@gmail.com>
2022-12-23 11:09:55 -0400

    teste edge 3

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 
 export async function getStaticProps() {
-  // let resposta = []
-  // try{
-  //   const urlLocal = "http://localhost:3000/"
-  //   resposta = await fetch(urlLocal +  "api/home")
-  // }catch(e){
-  //  const url = "https://turbo-octo-eureka.pages.dev/"
-  //   resposta = await fetch(url +  "api/home")
-  // }
-  // const list = await resposta.json()
+  let resposta = []
+  try{
+    const urlLocal = "http://localhost:3000/"
+    resposta = await fetch(urlLocal +  "api/home")
+  }catch(e){
+   const url = "https://turbo-octo-eureka.pages.dev/"
+    resposta = await fetch(url +  "api/home")
+  }
+  const list = await resposta.json()
 
-  const list = { destaques :[{id: 123}] }
+  // const list = { destaques :[{id: 123}] }
   return {
     props: list, 
   }
